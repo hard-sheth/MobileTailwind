baf0f3717c74e6feeb8b8f2837bbee4552de5c15
hard-sheth <hard.sheth@solutionanalysts.com>
2024-08-23 18:52:42 +0530

    Update profile screen.

--- screen/(Dashboard)/Profile.tsx
+++ screen/(Dashboard)/Profile.tsx
@@ -1,13 +1,16 @@
 import { View, Text, useColorScheme, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Header from '../../screencomponents/CustomToggler';
 
 const Profile = () => {
     const isDarkMode = useColorScheme() === 'dark';
     return (
         <View className={`flex-1 ${!isDarkMode ? 'bg-gray-800' : 'bg-slate-100'} `}>
-            <Text>Profile</Text>
-            <View 
-            className= 'absolute bottom-0 left-0 right-0 p-3'
+            <Header />
+            <Text className={`${isDarkMode ? 'text-gray-800' : 'text-slate-100'}`}>Profile</Text>
+            <Text className={`${isDarkMode ? 'text-gray-800' : 'text-slate-100'}`}>lorem10</Text>
+            <View
+                className={`absolute bottom-0 left-0 right-0 p-3 ${isDarkMode ? 'bg-gray-800' : 'bg-slate-100'}`}
             >
                 <TouchableOpacity onPress={() => alert('Update Content')} className={`bg-[#007AFF] rounded py-2`}>
                     <Text className='text-white text-center text-base'>Update Test Drawer</Text>
